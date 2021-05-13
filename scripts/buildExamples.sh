f637887477d5dd21e8aa50e13b832ff6516f57eb
Adil Malik <Adil_Malik@intuit.com>
2021-05-13 15:08:58 -0600

    Fix build errors

---
+++ scripts/buildExamples.sh
@@ -0,0 +1,5 @@
+cd ./examples/android-material-ui && yarn build:storybook
+cd ./examples/controls && yarn build:storybook
+cd ./examples/cross-platform && yarn build:storybook
+cd ./examples/flutter && yarn build:storybook
+cd ./examples/ios-material-ui && yarn build:storybook
