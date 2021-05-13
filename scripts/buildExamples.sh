4b939c25b01f3ae20746cdcc4cf5ff24ea931c1b
Adil Malik <25858348+amalik2@users.noreply.github.com>
2021-05-13 15:23:12 -0600

    Fix build errors (#59)
    * Fix build errors
    
    * Fix build errors
    
    * Fix build errors
    
    * Fix build errors
    
    Co-authored-by: Adil Malik <Adil_Malik@intuit.com>

---
+++ scripts/buildExamples.sh
@@ -0,0 +1,5 @@
+cd ./examples/android-material-ui && yarn build:storybook
+cd ../controls && yarn build:storybook
+cd ../cross-platform && yarn build:storybook
+cd ../flutter && yarn build:storybook
+cd ../ios-material-ui && yarn build:storybook
