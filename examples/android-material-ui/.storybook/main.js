70a78c35de1cad0bf75c38c23b3a93e930f2c6bb
brocollie08 <sentony93@gmail.com>
2021-08-04 01:19:51 -0400

    generate story with control

--- examples/android-material-ui/.storybook/main.js
+++ examples/android-material-ui/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stories: ["../stories/*.stories.jsx"],
+    stories: ["../stories/*.playground.stories.jsx"],
     addons: [
         "@storybook/addon-docs",
         "@storybook/addon-controls",
--- examples/android-material-ui/generate.js
+++ examples/android-material-ui/generate.js
@@ -1,12 +1,19 @@
-const { generateStories } = require("@storybook/native");
+const { generateStories, generateControlledStories } = require("@storybook/native");
 const { pascalCase } = require("change-case");
 const axios = require("axios");
 
 const components = [
     {
         name: "button",
-        docs:
-            "https://raw.githubusercontent.com/material-components/material-components-android/master/docs/components/Button.md"
+        control: {
+            label: "",
+            variant: ["primary", "passive", "special", "destructive"],
+            size: ["small", "medium", "large"],
+            fill: ["solid", "ghost", "text"],
+            something: false
+        },
+        docs: "https://raw.githubusercontent.com/material-components/material-components-android/master/docs/components/Button.md"
+
     },
     {
         name: "floatingButton",
@@ -60,25 +67,28 @@ const components = [
     }
 ];
 
-const promises = components.map(async (component) => {
-    const docsRequest = await axios.get(component.docs);
-    return generateStories({
-        category: pascalCase(component.name),
-        filePath: `./stories/${component.name}.stories.jsx`,
-        apiKey: "zv034bdme9je7c9d43chzmc2yg",
-        platform: "android",
-        stories: [
-            {
-                name: "Example",
-                appParams: {
-                    component: component.name
-                },
-                docs: docsRequest.data
-            }
-        ],
-        deepLinkUrl: "sb-native://deep.link"
+const promises = components
+    .filter((x) => x.control != undefined)
+    .map(async (component) => {
+        const docsRequest = await axios.get(component.docs);
+        return generateControlledStories({
+            category: pascalCase(component.name),
+            filePath: `./stories/${component.name}.playground.stories.jsx`,
+            apiKey: "zv034bdme9je7c9d43chzmc2yg",
+            platform: "android",
+            stories: [
+                {
+                    name: "Example",
+                    appParams: {
+                        component: component.name
+                    },
+                    docs: docsRequest.data
+                }
+            ],
+            controls: Object.entries(component.control),
+            deepLinkUrl: "sb-native://deep.link"
+        });
     });
-});
 
 Promise.all(promises).catch((err) => {
     console.error(err);
